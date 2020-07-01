08fe57654b00b6df2a4fea18a1ac968360351315
Boatdk <boatz.dk@gmail.com>
2020-07-01 10:09:39 +0700

    add login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -56,23 +56,6 @@ const Login = () => {
         </div>
       </Card>
       </div>
-        // <div className="login-img">
-        //   <div className="login-background">
-        //     <div className="login">
-        //       <h1>Login</h1>
-        //       <form>
-        //         <input type="text" name="userEmail"  placeholder="Username" required="required" />
-        //         <input type="password" name="userPassword"  placeholder="Password" required="required" />
-        //         <button type="submit" className="btn btn-primary btn-block btn-large">Login</button>
-        //       </form>
-        //       <br />
-        //       or
-        //       <div>
-        //         <button className="loginBtn loginBtn--facebook">Login with facebook</button>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </div>
       )
 }
 
